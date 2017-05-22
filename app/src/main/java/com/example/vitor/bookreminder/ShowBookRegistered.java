8fbc5c9ec0ae2d70a43899ab9d6239aa3c55ba76
Victor Costa <victorcostanascimento@hotmail.com>
2017-05-22 09:41:33 -0400

    Creating a bookmarking form and add a new Activity

--- app/src/main/java/com/example/vitor/bookreminder/ShowBookRegistered.java
+++ app/src/main/java/com/example/vitor/bookreminder/ShowBookRegistered.java
@@ -3,14 +3,11 @@ package com.example.vitor.bookreminder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+public class ShowBookRegistered extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_show_book_registered);
     }
 }
